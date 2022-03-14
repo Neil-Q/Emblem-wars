68f777447706fcf6a843c4f72ab1438f65095996
Neil-Q <neil.quechon@gmail.com>
2022-03-14 17:59:13 +0100

    Setting up project base

---
+++ src/components/SummonerGame.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="summonerGame">
+        <canvas id="mapLayer" width="640" height="448"></canvas>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SummonerGame"
+}
+</script>
+
+<style>
+
+#summonerGame {
+    width: 640px;
+    height: 448px;
+    margin: auto;
+}
+
+#mapLayer {  
+    position: relative;
+    left: 0;
+    top: 0;
+    z-index: 0;
+    background-color: beige;
+}
+
+</style>
